c5353f6a231efdad1e983f73240e7f151a1023fc
lcagma <puredzom1234@gmail.com>
2022-04-25 00:35:58 +0800

    to lower case and translate

--- app_core.py
+++ app_core.py
@@ -14,13 +14,16 @@ from flask import render_template
 
 from utils import get_qa_dict
 import config
+from googletrans import Translator
 
 
 app = Flask(__name__)
 
+
 line_bot_api = LineBotApi(config.CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(config.CHANNEL_SECRET)
 QA_dict = get_qa_dict(config.CONTEXT_PATH)
+language_list = config.LANGUAGE_LIST
 
 
 @app.route("/callback", methods=['POST'])
@@ -41,23 +44,33 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message_text_list = event.message.text.upper().split(' ')
+    message_text_list = event.message.text.lower().split(' ')
     reply_text = ""
     find_token = False
+    
     for message_text in message_text_list:
         if message_text in QA_dict:
             for answer in QA_dict[message_text]:
                 reply_text = reply_text + answer + '\n'
                 find_token = True
+        if message_text in language_list:
+            language = message_text
+
     if not find_token:
         reply_text = "Sorry, there is no answer to " + "\"" + event.message.text + "\""
+
+    if language is not None:
+        translator = Translator()
+        reply_text = translator.translate(reply_text, dest=language)
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text)
     )
-        
+
+    
 if __name__ == "__main__":
-    test()
     app.run()
